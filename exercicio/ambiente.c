0871a5025fb968eed7e52f1866b445b7d7dc97db
Nathan Canedo <101233593+nathancanedo@users.noreply.github.com>
2022-11-05 12:50:22 -0300

    Update ambiente.c

--- exercicio/ambiente.c
+++ exercicio/ambiente.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <locale.h>
 
-unsigned int idade = 39;
-char nome[] = "Gustavo Guanabara";
-float peso = 97.5;
-char sexo = 'M';
-
 void main() {
     setlocale(LC_ALL, "Portuguese");
-    printf("O %s tem %i anos de idade.\n", nome, idade);
-    printf("Seu peso atual é de %6.2fKg\n", peso);
-    printf("O seu sexo é %c", sexo);
+    unsigned int idade = 33;
+    float peso = 85.9;
+    char sexo = 'M';
+    char *nome = "Juvenal";
+
+    printf("%s é do sexo %c, tem %i anos e pesa %.2fKg", nome, sexo, idade, peso);
 }
