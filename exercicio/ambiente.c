7964354481bd8fa745651c17555bb5891f3494fe
Nathan Canedo <101233593+nathancanedo@users.noreply.github.com>
2022-11-05 14:11:40 -0300

    Update ambiente.c

--- exercicio/ambiente.c
+++ exercicio/ambiente.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 #include <locale.h>
+#define TOTAL 22
 
 void main() {
     setlocale(LC_ALL, "Portuguese");
-    unsigned int idade = 33;
-    float peso = 85.9;
-    char sexo = 'M';
-    char *nome = "Juvenal";
-
-    printf("%s é do sexo %c, tem %i anos e pesa %.2fKg", nome, sexo, idade, peso);
+    printf("%d", TOTAL);
 }
