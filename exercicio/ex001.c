e119ee94a474f9f943adbdf445025133dd6dcc3d
Nathan Canedo <101233593+nathancanedo@users.noreply.github.com>
2022-11-06 11:09:20 -0300

    Update ex001.c

--- exercicio/ex001.c
+++ exercicio/ex001.c
@@ -5,10 +5,11 @@ void main() {
     setlocale(LC_ALL, "Portuguese");
     printf("<<< EX0001 - Listagem) >>>\n\n");
     printf("Listagem de Alunos\n");
-    printf("Nome \t\tNota \n");
+    printf("Nome \t\tNota\n");
     printf("------------------------ \n");
-    printf("Ana Beatriz \t 8.5\n");
-    printf("Bianca Martins \t 9.0\n");
-    printf("Cláudio Sá \t 5.5\n");
-    printf("Giovana Silva \t 7.5");
+    printf("Ana Beatriz\t8.5\n");
+    printf("Bianca Martins\t9.0\n");
+    printf("Cláudio Sá\t5.5\n");
+    printf("Giovana Silva\t7.5");
+    printf("\n------------------------ \n");
 }
